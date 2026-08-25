d8d58689114b4e4d941b0a6a09d1e15e18eb2392
Charvi khandelwal <khandelwalcharvi@gmail.com>
2026-08-25 15:29:58 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        int l= s.length();
+        int m= t.length();
+        if(l!=m)
+            return false;
+        unordered_map<char,int> mpp1;
+        unordered_map<char,int> mpp2;
+        for(int i=0;i<l;i++)
+        {
+            mpp1[s[i]]++;
+        }
+        for(int i=0;i<m;i++)
+        {
+            mpp2[t[i]]++;
+        }
+        for(int i=0;i<l;i++)
+        {
+            if(mpp2.find(s[i])!= mpp2.end())
+            {
+                if(mpp2[s[i]]==0)
+                    return false;
+                mpp2[s[i]]--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
